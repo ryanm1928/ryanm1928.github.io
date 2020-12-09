4baa9118854cc92d81648919454948dbd939a2a9
ryanm1928 <70052352+ryanm1928@users.noreply.github.com>
2020-12-09 10:25:53 +0700

    Add files via upload

---
+++ js/quotes.js
@@ -0,0 +1,43 @@
+var quotes = [
+
+{
+    quote: " ~ Setiap orang di negara ini harus belajar memprogram komputer, karena itu mengajari Anda cara berpikir ~",
+    source: "Steve Jobs",
+},
+{
+    quote: " ~ Hal terbaik tentang boolean adalah meskipun Anda salah, Anda hanya meleset sedikit ~ ",
+    source: " Anonymous",
+},
+{
+    quote: " ~ Software itu seperti sex: Lebih baik kalo bebas ~ ",
+    source: "Linus Torvalds",
+},
+{
+    quote: " ~ Pacaran  itu sama anak Proggrammer titik koma aja di perhatiin apa lagi kamu ~ ",
+    source: "Anak baru masuk jurusan IT",
+},
+{
+    quote: " ~ Mengukur pembuatan program berdasarkan jumlah baris kode sama seperti mengukur pembuatan pesawat terbang berdasarkan beratnya ~ ",
+    source: "Bill Gates",
+},
+{
+    quote: " ~ Jangan Lupa titik koma (;) ~ ",
+    source: "Shandika Galih (Web Proggraming UNPAS) ",
+},
+{
+    quote: " ~ Bang bisa hack akun Facebook temen saya gak ~ ",
+    source: ":)",
+},
+{
+    quote: " ~ Jika kau tidak tahu tentang suatu hal maka belajarlah ~ ",
+    source: "me",
+},
+{
+    quote: " ~ 80% Proggrammer mencari tutorial di youtube pasti pernah ketemu teacher India ~ ",
+    source: "aku yang menikmati logat india",
+},
+{
+    quote: " ~ Mending Rakit PC ~ ",
+    source: "Jalan Ninja ku",
+}
+];
